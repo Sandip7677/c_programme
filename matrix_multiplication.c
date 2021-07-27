57d4cff4d5086d60ab590b0aea674765d0414b84
anuj <anuj7870040353@gmail.com>
2021-07-27 09:43:38 +0530

    added new file

--- matrix_multiplication.c
+++ matrix_multiplication.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include<string.h>
 
 int main()
 {
@@ -12,6 +13,7 @@ int main()
     if (n1 == m2)
     {
         int resultant_matrix[m1][n2];
+        memset(resultant_matrix,0,m1*n2*sizeof(resultant_matrix[0][0]));
         printf("input number in matrix 1 of order %d x %d\n", m1, n1);
         for (int i = 0; i < m1; i++)
         {
@@ -50,9 +52,12 @@ int main()
 
         for (int i = 0; i < m1; i++)
         {
-            for (int j = 0; j < n1; j++)
+            for (int j = 0; j < n2; j++)
             {
-                matrix1[i][j] = matrix1[i][j] + matrix2[i][j];
+                for (int k = 0; k < n1; k++)
+                {
+                    resultant_matrix[i][j] += matrix1[i][k] * matrix2[k][j];
+                }
             }
         }
         printf("resultant matrix is \n");
